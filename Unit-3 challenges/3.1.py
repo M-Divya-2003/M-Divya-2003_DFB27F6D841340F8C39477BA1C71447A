d05bf0a4408ee87089b4c21ec3ffd2e194812353
M-Divya-2003 <144591506+M-Divya-2003@users.noreply.github.com>
2023-10-05 19:38:19 +0530

    Add files via upload

---
+++ Unit-3 challenges/3.1.py
@@ -0,0 +1,22 @@
+"""Perform a linear search to find occurrences of the target product in the list.
+
+    Parameters:
+    product_list (list): A list of product names.
+    target_product (str): The product name to search for.
+
+    Returns:
+    list: A list of indices of occurrences of the target product, or an empty list if not found."""
+def linear_search_product(product_list, target_product):
+    occurrences = []
+
+    for index, product in enumerate(product_list):
+        if product == target_product:
+            occurrences.append(index)
+
+    return occurrences
+
+# Example usage
+products = ["Apple", "Banana", "Orange", "Apple", "Banana"]
+target_product = "Apple"
+result = linear_search_product(products, target_product)
+print(target_product, ":", result)
